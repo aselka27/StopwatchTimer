3c3354acfbfbcf9f157dec5b46be5af09d5c21af
aselka27 <asjumalieva@gmail.com>
2023-04-13 14:37:33 -0500

    Initial Commit

---
+++ StopwatchTimer/StopwatchTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchTimerApp.swift
+//  StopwatchTimer
+//
+//  Created by саргашкаева on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
